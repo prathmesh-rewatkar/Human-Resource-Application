8c7eba44c2f1a0fe437d5fcd48afbbdd1107fe96
Prathmesh2011 <prathmeshrewatkar2011@gmail.com>
2026-03-25 15:46:21 +0530

    initial commit

---
+++ src/main/java/com/example/HumanResourceApplication/HumanResourceApplication.java
@@ -0,0 +1,14 @@
+package com.example.HumanResourceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(HumanResourceApplication.class, args);
+	}
+
+}
