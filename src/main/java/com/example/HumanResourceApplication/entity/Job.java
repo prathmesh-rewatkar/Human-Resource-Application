3406b907f3de2f946956a25f509a65cda70ce4ab
nidhi <nidhichoudhari023@gmail.com>
2026-03-26 12:48:00 +0530

    fix jobId type and mapping

--- src/main/java/com/example/HumanResourceApplication/entity/Job.java
+++ src/main/java/com/example/HumanResourceApplication/entity/Job.java
@@ -4,14 +4,22 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(name = "jobs")   
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Job {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // ✅ AUTO INCREMENT
-    private Long jobId;
+    @Column(name = "job_id")
+    private String jobId;
 
+    @Column(name = "job_title")
     private String jobTitle;
+
+    @Column(name = "min_salary")
+    private Double minSalary;
+
+    @Column(name = "max_salary")
+    private Double maxSalary;
 }
