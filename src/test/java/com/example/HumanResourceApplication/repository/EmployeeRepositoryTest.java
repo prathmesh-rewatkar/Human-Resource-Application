b6aa133a58b73afceea27ea97d7c3ffaa4c9f56f
Prathmesh2011 <prathmeshrewatkar2011@gmail.com>
2026-03-27 15:18:17 +0530

    added get employees test cases

--- src/test/java/com/example/HumanResourceApplication/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/example/HumanResourceApplication/repository/EmployeeRepositoryTest.java
@@ -28,4 +28,54 @@ public class EmployeeRepositoryTest {
         assertThat(employees).isNotNull();
         assertThat(employees.size()).isGreaterThan(0);
     }
+
+    @Test
+    @DisplayName("findAll - validate employee fields")
+    void testFindAll_ValidateEmployeeFields() {
+
+        List<Employee> employees = employeeRepository.findAll();
+
+        Employee employee = employees.get(0);
+
+        assertThat(employee.getEmployeeId()).isNotNull();
+        assertThat(employee.getFirstName()).isNotNull();
+        assertThat(employee.getLastName()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("findAll - should return consistent size")
+    void testFindAll_SizeConsistency() {
+
+        List<Employee> employees1 = employeeRepository.findAll();
+        List<Employee> employees2 = employeeRepository.findAll();
+
+        assertThat(employees1.size()).isEqualTo(employees2.size());
+    }
+
+    @Test
+    @DisplayName("findAll - employee IDs should be unique")
+    void testFindAll_UniqueIds() {
+
+        List<Employee> employees = employeeRepository.findAll();
+
+        long uniqueCount = employees.stream()
+                .map(Employee::getEmployeeId)
+                .distinct()
+                .count();
+
+        assertThat(uniqueCount).isEqualTo(employees.size());
+    }
+
+    @Test
+    @DisplayName("findAll - employees should have valid data")
+    void testFindAll_ValidData() {
+
+        List<Employee> employees = employeeRepository.findAll();
+
+        employees.forEach(emp -> {
+            assertThat(emp.getEmployeeId()).isNotNull();
+            assertThat(emp.getFirstName()).isNotBlank();
+            assertThat(emp.getLastName()).isNotBlank();
+        });
+    }
 }
