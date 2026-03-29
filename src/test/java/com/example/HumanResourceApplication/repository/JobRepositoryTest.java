2d7ebdaefaf09bd34545b18f4ef54df92006c940
Swaralifendar <swaralifendar@gmail.com>
2026-03-29 14:55:03 +0530

    Added certain api test cases and changes

--- src/test/java/com/example/HumanResourceApplication/repository/JobRepositoryTest.java
+++ src/test/java/com/example/HumanResourceApplication/repository/JobRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 
@@ -12,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class JobRepositoryTest {
 
     @Autowired
